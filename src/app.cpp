94250a9984514f441a2e5c261a3e316d9770e8c2
cxxuniverse <anshchadha1996@gmail.com>
2024-05-12 17:24:19 +0200

    draw a triangle app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -14,10 +14,10 @@ void App::create_window(const char *title, int width, int height)
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,
+                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
     gl_context = SDL_GL_CreateContext(window);
-    // SDL_GL_CreateContext(window);
 }
 
 void App::game_loop()
@@ -36,6 +36,14 @@ void App::game_loop()
         // clear the window
         glClearColor(0.7f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+
+        glEnd();
 
         // update window with OpenGL rendering, used for double-buffered frame(buffer)
         SDL_GL_SwapWindow(window);
